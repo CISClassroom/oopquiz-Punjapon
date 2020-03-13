e41184e053296644c95661a692d09f2e4bea6ece
methaweesareephol <33540522+methaweesareephol@users.noreply.github.com>
2020-03-13 16:00:09 +0700

    Quiz

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Student ;
+        String Major;
+        String University;
+        String  Personnel ;
+        String Department ;
+        int register ;
+
+         canregister():Boolean
+        Register(PersonPerson):Boolean
+}
+}
---
+++ src/com/company/Register.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Register {
+
+    public String Student;
+    public String  Major ;
+    public String  University ;
+    public String  Personnel ;
+    public String  Department ;
+    public int Registered ;
+
+}
+int Registered.
+ public boolean Register () {
+     if (Registered < 46)
+         return true;
+     System.out.println("ลงทะเบียนได้");
+    else (Registered > 45 )
+             return false;
+     System.out.println("ไม่สามารถลงทะเบียนได้ที่นั่งเต็มแล้ว");
+ }
